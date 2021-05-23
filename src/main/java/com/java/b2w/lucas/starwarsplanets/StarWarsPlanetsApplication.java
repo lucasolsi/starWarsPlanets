e7c5e44d6b8a54d1c60453da5b75e9d27f8fcab3
Lucas Oliveira Silva <lucasolsi@gmail.com>
2021-05-22 22:59:26 -0300

    Initial commit

---
+++ src/main/java/com/java/b2w/lucas/starwarsplanets/StarWarsPlanetsApplication.java
@@ -0,0 +1,15 @@
+package com.java.b2w.lucas.starwarsplanets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StarWarsPlanetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/b2w/lucas/starwarsplanets/StarWarsPlanetsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.java.b2w.lucas.starwarsplanets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
