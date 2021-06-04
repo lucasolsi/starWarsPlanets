f0f880f77d6648bc146c77239c8ba920f74c57b8
Lucas Oliveira Silva <lucasolsi@gmail.com>
2021-06-03 21:13:45 -0300

    Removing CommandLineRunner

--- src/main/java/com/java/b2w/lucas/starwarsplanets/StarWarsPlanetsApplication.java
+++ src/main/java/com/java/b2w/lucas/starwarsplanets/StarWarsPlanetsApplication.java
@@ -1,41 +1,13 @@
 package com.java.b2w.lucas.starwarsplanets;
 
-import com.java.b2w.lucas.starwarsplanets.model.entity.PlanetEntity;
-import com.java.b2w.lucas.starwarsplanets.repository.PlanetRepository;
-import com.java.b2w.lucas.starwarsplanets.utils.RestClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StarWarsPlanetsApplication implements CommandLineRunner
+public class StarWarsPlanetsApplication
 {
-
-    @Autowired
-    PlanetRepository planetRepository;
-
     public static void main(String[] args)
     {
         SpringApplication.run(StarWarsPlanetsApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception
-    {
-        planetRepository.deleteAll();
-        PlanetEntity planet1 = new PlanetEntity()
-                .withName("Tatooine")
-                .withClimate("arid")
-                .withTerrain("desert");
-        planet1.setNumberOfFilms(RestClient.findNumberOfFilms(planet1.getName()));
-        planetRepository.save(planet1);
-
-        PlanetEntity planet2 = new PlanetEntity()
-                .withName("Hoth")
-                .withClimate("temperate, tropical")
-                .withTerrain("jungle, rainforests");
-        planet2.setNumberOfFilms(RestClient.findNumberOfFilms(planet2.getName()));
-        planetRepository.save(planet2);
     }
 }
